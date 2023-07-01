29208fa7c634f7ab00126af991b3b7b52d00cd47
rk14283 <rk14283@my.bristol.ac.uk>
2023-07-01 13:26:09 +0100

    new switch challenge

---
+++ ASwitchExpressionChallenge/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+//Day of the week challenge
+//1. Create a method called printDayOfWeek, that takes as an int parameter called day, but
+//doesn't return any values.
+//  Use the enhanced switch statement, to return the name of the day, based on the parameter
+//passed to the switch statement, so that 0 will return "Sunday", 1 will return "Monday",
+//and so on. Any number not between 0 and 6, should return "Invalid Day".
+// Use the enhanced switch statement as an expression, returning the result to a String named dayOfTheWeek
+//Print both the day variable and the dayOfTheWeek variable
+
+//2. In the main method, call this method for values 0 through 7.
+//Bonus: Create a second method called printWeekDay, that uses an if then else statement, instead of a switch.
+//to produce the same output.
+
+
