ff61dc92b694993d1c3294ecf2af1378fd85cfdb
rk14283 <rk14283@my.bristol.ac.uk>
2023-07-02 11:45:56 +0100

    new switch, but solution works with old switch

--- printNumberInWord/src/Main.java
+++ printNumberInWord/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printNumberInWord(-1);
+        printNumberInWord(0);
+
+    }
+
+    public static void printNumberInWord(int number) {
+        String text = switch (number) {
+            case 0 -> "Zero";
+            case 1 -> "One";
+            case 2 -> "Two";
+            case 3 -> "Three";
+            case 4 -> "Four";
+            case 5 -> "Five";
+            case 6 -> "Six";
+            case 7 -> "Seven";
+            case 8 -> "Eight";
+            case 9 -> "Nine";
+            default -> "other";
+        };
+        System.out.println(text);
     }
 }
