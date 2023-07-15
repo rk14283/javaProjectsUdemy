1c91afeb97597b667cee7f9b97c840026b255277
rk14283 <rk14283@my.bristol.ac.uk>
2023-07-15 21:46:44 +0100

    inheritance

---
+++ Inheritance/src/Dog.java
@@ -0,0 +1,65 @@
+public class Dog extends Animal{
+
+    private String earShape;
+    private String tailShape;
+
+
+
+
+    public Dog() {
+        super("Mutt", "Big", 50);
+
+    }
+    public Dog(String type, double weight){
+            this(type, weight, "Perky", "Curled");
+        }
+
+
+
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight <  15 ? "small" : (weight < 35 ? "medium" : "large"),
+                weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+//very important
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+
+
+          }
+
+   public void makeNoise(){
+
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        System.out.println("Dogs run and wag their tail");
+    }
+}
+
+//Code Re-use
+//All subclasses can execute methods, even though the code is declared on the parent class.
+// The code doesn't have to be duplicated in each subclass.
+//We can use code, from the parent.
+//Or we can change the code or the subclass
+
+//Overriding
+//Overriding a method is when you create a method on a subclass, which has the same signature as a method on a superclass
+//You override a parent class method when you want the child class to show different behavior for that method
+
+//Overridden method
+//The overridden method can do one of three things:
+//It can implement completely different behavior, overriding the behavior of the parent.
+//It can simply call the parent class's method, which is somewhat redundant to do.
+//Or the method can call the parent class's method, and include other code to run, so it
+//can extend the functionality for the Dog, for that behavior.
+
---
+++ Inheritance/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        Animal animal = new Animal ("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie,"fast");
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
+    }
+    //it is static because we are calling it from the main method 
+    public static void doAnimalStuff(Animal animal, String speed){
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_____");
+
+    }
+}
