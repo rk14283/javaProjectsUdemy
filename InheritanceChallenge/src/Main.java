951dc53806cfc22b3768609d2801467366469f4a
rk14283 <rk14283@my.bristol.ac.uk>
2023-07-26 10:08:04 +0100

    inheritance challenge 1

---
+++ InheritanceChallenge/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
+        System.out.println(tim);
+        System.out.println("Age = " +tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
+
+        Employee joe = new Employee("Joe", "11/11/1990", "03/03/2020");
+        System.out.println(joe);
+
+    }
+}
+
+
+//A static field let's you share data among all the instances 
